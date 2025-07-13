906fb11d328666d1ac76a4abb8bae49d382d8b66
chitranjan singh <singhchitranjan739@gmail.com>
2025-07-13 22:16:48 +0530

    details section added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+// App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Product from './Components/product';
+import Detail1 from './Components/productDetails/detail1';
+import Detail2 from './Components/productDetails/detail2';
+import Detail3 from './Components/productDetails/detail3';
+import Detail4 from './Components/productDetails/detail4';
 
 function App() {
   return (
-    <div>
-      <Product />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Product />} />
+       <Route path="/product-detail/detail1" element={<Detail1 />} />
+{/* <Route path="/product-detail/detail2" element={<Detail2 />} />
+<Route path="/product-detail/detail3" element={<Detail3 />} />
+<Route path="/product-detail/detail4" element={<Detail4 />} /> */}
+
+      </Routes>
+    </Router>
   );
 }
 
